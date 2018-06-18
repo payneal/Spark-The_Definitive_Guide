c3d86d6e88370d9c3c1d72bde829440dd8c47da2
Ali Payne <Ali.Payne.CW@dunnhumby.com>
2018-06-18 22:25:09 +0100

    moved to part 2 of book

--- Part_1/3_A_Tour_of_Sparks_toolset/test_example.py
+++ Part_1/3_A_Tour_of_Sparks_toolset/test_example.py
@@ -2,10 +2,14 @@
 from pyspark import SparkConf
 from pyspark.sql  import SparkSession
 import unittest
-from pyspark.sql.functions import window, column, desc, col
+from pyspark.sql.functions import window, column, desc, col, date_format
 from datetime import timedelta
 from pyspark.sql.types import *
 import time
+from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler
+from pyspark.ml import Pipeline
+from pyspark.ml.clustering import KMeans
+
 
 class test_chapter_3(unittest.TestCase):
     
@@ -23,15 +27,14 @@ class test_chapter_3(unittest.TestCase):
             StructField("Quantity", IntegerType(), True),
             StructField("InvoiceDate", TimestampType(), True),
             StructField("UnitPrice", DoubleType(), True),
-            StructField("CustomerID", DoubleType(), True),
+            StructField("CustomerId", DoubleType(), True),
             StructField("Country", StringType(), True)
         ])
         
     def tearDown(self):
         pass
 
-
-    def get_stream(self):
+    def get_stream_df(self):
         return self.spark.readStream\
             .schema(self.staticSchema)\
             .option("maxFilesPerTrigger", 1)\
@@ -39,11 +42,14 @@ class test_chapter_3(unittest.TestCase):
             .option("header", "true")\
             .load("/data/retail-data/by-day/*.csv")
 
-    def test_example_of_of_using_read(self):
-        staticDataFrame = self.spark.read.format('csv')\
+    def get_static_df(self):
+        return self.spark.read.format('csv')\
             .option("header", "true")\
             .option("inferSchema", "true")\
             .load("./data/retail-data/by-day/*.csv")
+
+    def test_example_of_of_using_read(self):
+        staticDataFrame = self.get_static_df()
         staticDataFrame.createOrReplaceTempView("retail_data")
         #  this is for getting the scheama for streaming example
         staticSchema = staticDataFrame.schema
@@ -52,19 +58,17 @@ class test_chapter_3(unittest.TestCase):
             staticDataFrame.columns[0])
     
     def test_window_functions(self):
-        staticDataFrame = self.spark.read.format('csv')\
-            .option("header", "true")\
-            .option("inferSchema", "true")\
-            .load("./data/retail-data/by-day/*.csv")
+        staticDataFrame = self.get_static_df() 
         df = staticDataFrame.selectExpr(
-            "CustomerId", 
+            "CustomerID", 
             "(UnitPrice * Quantity) as total_cost", 
             "InvoiceDate").groupBy(    
-                col("CustomerId"), window(col("InvoiceDate"), "1 day")
+                col("CustomerID"), window(col("InvoiceDate"), "1 day")
             ).sum("total_cost")
         info = df.limit(2).collect()
-        self.assertNotEqual(info[0]['CustomerId'], info[1]['CustomerId'])
-        # umm.show(20, False) 
+        self.assertNotEqual(info[0]['CustomerID'], info[1]['CustomerID'])
+        # print "this is in test window functions"
+        # df.show(20, False) 
         start = info[0]['window']['start'] 
         end = info[0]['window']['end'] 
         # test explains how window function worked in this example
@@ -72,49 +76,108 @@ class test_chapter_3(unittest.TestCase):
 
     
     def test_example_of_using_streams(self):
-        streamingDataFrame = self.get_stream()
+        streamingDataFrame = self.get_stream_df()
         # use this to see if data frame is streaming
         self.assertEqual(streamingDataFrame.isStreaming, True) 
     
     def test_streaming_with_summation(self):
-        streamingDataFrame = self.get_stream()
+        streamingDataFrame = self.get_stream_df()
         
-        
-        print "is streamingDataFram  streaming: {}".format(
-            streamingDataFrame.isStreaming)
-
-        
+        # print "is streamingDataFram  streaming: {}".format(
+        #    streamingDataFrame.isStreaming)
 
         purchaseByCustomerPerHour = streamingDataFrame\
             .selectExpr( 
                 "CustomerId",
                 "(UnitPrice * Quantity) as total_cost",
                 "InvoiceDate").groupBy(
-                    col("CustomerId"), window(col("InvoiceDate"), "1 day")
-                ).sum("total_cost")
+                    col("CustomerId"), window(col("InvoiceDate"), "1 day"))\
+            .sum("total_cost")
 
+        #start() starts the stream
         umm = purchaseByCustomerPerHour.writeStream\
             .format("memory")\
             .queryName("customer_purchases")\
             .outputMode("complete")\
             .start()
     
-        time.sleep(15)
-
-
-        umm.stop()
-
-        print  "this is umm: "
-        print umm
-
-        
+        # in real siutuation you can make this call
+        # in different terminal and it will show you
+        # df so far  while stream is happening
         self.spark.sql("""
             Select * 
             FROM customer_purchases
             Order by `sum(total_cost)` DESC
             """).show(5)
+
+        # couldn't get above dataframe to contain anything so it lead to the
+        # conclusion above hopefully Ill understand better with further
+        # reading
+
+
+        # stop the stream
+        umm.stop()
+        self.assertEqual(True, True)
+
+    def test_transformations_of_data_to_numerical_representation(self):
+        staticDataFrame = self.get_static_df() 
+        preppedDataFrame = staticDataFrame\
+            .na.fill(0)\
+            .withColumn(
+                "day_of_week", date_format(col("InvoiceDate"), "EEEE"))\
+            .coalesce(5)
+  
+        self.assertEqual(
+            len(staticDataFrame.columns) +1,
+            len(preppedDataFrame.columns))
+
+        # split data
+        trainData = preppedDataFrame\
+            .where("InvoiceDate < '2011-07-01'")
+        testData = preppedDataFrame\
+            .where("InvoiceDate >= '2011-07-01'")
         
-        self.assertEqual(True, False)
+        self.assertEqual(
+            preppedDataFrame.count(), 
+            trainData.count() + testData.count())
+    
+        # change from string to idx
+        indexer = StringIndexer()\
+            .setInputCol("day_of_week")\
+            .setOutputCol("day_of_week_index")
+
+        encoder = OneHotEncoder(
+            inputCol= "day_of_week_index", outputCol ="day_of_week_encoded")
+
+        # create a vector
+        vectorAssembler = VectorAssembler()\
+            .setInputCols(["UnitPrice", "Quantity", "day_of_week_encoded"])\
+            .setOutputCol("features")
+    
+        transformationPipeline = Pipeline()\
+            .setStages([indexer, encoder, vectorAssembler])
+
+        fittedPipeline = transformationPipeline.fit(trainData)
+
+        transformedTraining = fittedPipeline.transform(trainData)
+
+        transformedTraining.cache()
+
+        kmeans = KMeans().setK(20).setSeed(1L)
+        
+        kmModel = kmeans.fit(transformedTraining)
+
+        print "this is compute Cost for transformed trainning: {}".format(
+            kmModel.computeCost(transformedTraining))
+        transformedTest = fittedPipeline.transform(testData)
+        print "this is compute cost for transformed test: {}".format(
+            kmModel.computeCost(transformedTest))
+        
+        transformedTest.show(20, False)
+        
+        self.assertEqual(True, True)
+
+
 
 
 if __name__ == "__main__": 
---
+++ Part_2/4_Structured_api_overview/test_example.py
@@ -0,0 +1,15 @@
+import unittest
+
+class test_chapter_4(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_what_it_dew(self):
+        self.assertEqual(False, True)
+
+if __name__ == "__main__":
+    unittest.main()
